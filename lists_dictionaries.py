15b394dfb2ab2bedbb3fd7fba746669057246b03
abhixshek <abhishek.dce.2000@gmail.com>
2025-06-23 22:46:20 +0530

    list indexing and slicing

--- lists_dictionaries.py
+++ lists_dictionaries.py
@@ -16,4 +16,32 @@ print(a) # a was not changed
 
 # r = [1, 2, 3] + "45" # gives TypeError. you cant concatenate a list object and a str object
 
+a = [5,7,3,87,1,8,5]
+print(a)
+# iteration
+for i in a:
+    print(i, end=" ")
+
+print()
+print(3 in a) # membership
+
+r = [i * 2 for i in a] # list comprehension
+print(r)
+
+a = list(range(-4, 4))
+print(a)
+r = list(map(abs, a)) # map generator function applies the specified function to each item in the iterable
+print(r)
+
+print(a[0]) # indexing
+print(a[-2])
+print(a[2:4]) # slicing, includes index 2 and 3, not 4
+
+matrix = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]
+          ] # nested list, acts like a 2D array
+print(matrix)
+print(matrix[1])
+print(matrix[1][1])
 
