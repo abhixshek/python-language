85453ae84b8f40d8c2487f768937ad9e4fe3ff26
abhixshek <abhishek.dce.2000@gmail.com>
2025-11-12 21:25:43 +0530

    from ..import *

---
+++ modules_in_python/from_star.py
@@ -0,0 +1,5 @@
+from module1 import *
+
+
+printer('imported everything at the top level of the referenced module')
+
---
+++ modules_in_python/module3.py
@@ -0,0 +1,5 @@
+from module1 import printer
+
+
+printer('India is a country.')
+
