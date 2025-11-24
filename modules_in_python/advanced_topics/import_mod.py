f5033f29400031723d81703936afa66ffd58687a
abhixshek <abhishek.dce.2000@gmail.com>
2025-11-25 00:51:50 +0530

    __name__ attribute of modules

---
+++ modules_in_python/advanced_topics/import_mod.py
@@ -0,0 +1,4 @@
+import mod # did not call func since __name__ attribute is not __main__ when imported
+
+mod.func()
+
---
+++ modules_in_python/advanced_topics/mod.py
@@ -0,0 +1,10 @@
+# __name__ attribute of modules
+
+def func():
+    print("tester function")
+
+if __name__ == "__main__":
+    print("__name__ is __main__")
+    func()
+    print("program run complete.")
+
